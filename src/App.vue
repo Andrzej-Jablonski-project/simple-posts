caebfe44ce2f6d9f9cbba71aef9e79b951b9a2ff
Andrzej Jabłoński <andrzej@audioszum.pl>
2021-07-08 14:16:45 +0200

    add normalize.css

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,13 @@
 </template>
 
 <style lang="scss">
+@import "~normalize.css";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  box-sizing: border-box;
   text-align: center;
   color: #2c3e50;
 }
